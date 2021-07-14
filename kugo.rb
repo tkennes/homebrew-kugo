bc9f8633a88f524e59ff2ba84b420b97e9f09f4e
goreleaserbot <goreleaser@carlosbecker.com>
2021-07-14 19:33:46 +0200

    Brew formula update for kugo version v1.0.24

--- kugo.rb
+++ kugo.rb
@@ -5,28 +5,28 @@
 class Kugo < Formula
   desc "Golang based CLI for Jenkins API."
   homepage "https://github.com/tkennes/kugo"
-  version "1.0.21"
+  version "1.0.24"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "http://github.com/tkennes/kugo/releases/download/v1.0.21/kugo_1.0.21_Darwin_x86_64.tar.gz"
-      sha256 "25d620be9c5b7acc5a904b81d2392b44676fe1527926590e0f63f0029d871586"
+      url "http://github.com/tkennes/kugo/releases/download/v1.0.24/kugo_1.0.24_Darwin_x86_64.tar.gz"
+      sha256 "2b4d6d4aa08d2fb61b835629aac2f0b4343f98aef635f3dcc64bf4358c5ee40b"
     end
     if Hardware::CPU.arm?
-      url "http://github.com/tkennes/kugo/releases/download/v1.0.21/kugo_1.0.21_Darwin_arm64.tar.gz"
-      sha256 "03c2346403b09981d334fad589f3cde452b82364d19493a1869c515f7f31a717"
+      url "http://github.com/tkennes/kugo/releases/download/v1.0.24/kugo_1.0.24_Darwin_arm64.tar.gz"
+      sha256 "22dd0f705c16cde8eb10fdf9c01336c1341c092198a3f813d77bb07f1a497c59"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "http://github.com/tkennes/kugo/releases/download/v1.0.21/kugo_1.0.21_Linux_x86_64.tar.gz"
-      sha256 "d9a08c726a4684ede414be07d9e0216bc09db9598050546712b892d4b10f71ee"
+      url "http://github.com/tkennes/kugo/releases/download/v1.0.24/kugo_1.0.24_Linux_x86_64.tar.gz"
+      sha256 "2649263a4139a589602532bed59caf25fdc02f93bf8bd5a891ed408075da818a"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "http://github.com/tkennes/kugo/releases/download/v1.0.21/kugo_1.0.21_Linux_arm64.tar.gz"
-      sha256 "20bf3982fa6eae648b64380eeaca6825ada4bc7cd4dcda28afe46581dbd65fec"
+      url "http://github.com/tkennes/kugo/releases/download/v1.0.24/kugo_1.0.24_Linux_arm64.tar.gz"
+      sha256 "5d3c8c6d31acca3fe6f3182869fea12e556d6a7f8c9fe44de9bca0c4553c9775"
     end
   end
 
